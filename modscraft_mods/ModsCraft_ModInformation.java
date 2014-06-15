4c26af5a669e5e3a7cd4fbf2720bec5b0b8a5214
Michael Poutanen <michael.poutanen@googlemail.com>
2014-06-15 04:26:17 +0200

    Set it to working state in eclipse - its now clonable and you can build the client to use it.

--- modscraft_mods/ModsCraft_ModInformation.java
+++
@@ -1,8 +0,0 @@
-package modscraft_mods;
-
-public class ModsCraft_ModInformation {
-	public static final String ID = "ModsCraft_Mods";
-    public static final String name = "ModsCraft.eu Mod collection";
-    public static final String version = "0.1";
-    public static final String channel = "modscraft.eu_comChannel";
-}
